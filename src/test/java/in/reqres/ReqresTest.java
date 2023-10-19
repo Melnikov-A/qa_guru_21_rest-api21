b6b7a00e1058f0fafd47274e7c6f2398029b3010
Anton Melnikov <101597291+Melnikov-A@users.noreply.github.com>
2023-10-20 01:15:41 +0300

    add rest api

---
+++ src/test/java/in/reqres/ReqresTest.java
@@ -0,0 +1,106 @@
+package in.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class ReqresTest extends TestBase {
+    @Test
+    void successfulLoginTest() {
+
+        String authData = "{\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+
+    }
+    @Test
+    void listUsersTest() {
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", hasItem("Michael"));
+
+    }
+
+    @Test
+    void deleteUsersTest() {
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .delete("/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+    }
+    @Test
+    void singleResourceTest() {
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.year",is(2001))
+                .body("support.text", is ("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+    }
+
+    @Test
+    void unsuccessfulLoginTest() {
+
+        String authData = "{\n" +
+                "    \"email\": \"peter@klaven\"\n" +
+                "}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+
+}
